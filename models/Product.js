1fde6c93ef3bbbb60819a4c8cd0b975014b2d1ce
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-10 19:24:11 -0400

    Included 'product_name', 'price', and 'stock' columns with appropriate data types and constraints in Product model setup.

--- models/Product.js
+++ models/Product.js
@@ -16,6 +16,29 @@ Product.init(
       primaryKey: true,
       autoIncrement: true,
     },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    product_name: {
+      type: DataTypes.STRING,
+      allowNull: false,
+    },
+    price: {
+      type: DataTypes.DECIMAL,
+      allowNull: false,
+      validate: {
+        isDecimal:true
+      },
+    },
+    stock: {
+      type: DataTypes.INTEGER,
+      allowNull: false,
+      defaultValue: 10,
+      validate: {
+        isNumeric:true
+    }
+  },
 },
   {
     sequelize,
