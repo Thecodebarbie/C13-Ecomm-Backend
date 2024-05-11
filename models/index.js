79da0b0d9b079d730e5aed70354f850c1ea5f5c7
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-10 20:12:56 -0400

    Created a 'belongsToMany' relationship between Product and Tag models, using 'ProductTag' as the intermediary table for their associations.

--- models/index.js
+++ models/index.js
@@ -14,7 +14,14 @@ Category.hasMany(Product, {
   onDelete: 'CASCADE',
 });
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  // Define the third table needed to store the foreign keys
+  through: {
+    model: ProductTag,
+    unique: false
+  },
+ 
+});
 // Tags belongToMany Products (through ProductTag)
 
 module.exports = {
