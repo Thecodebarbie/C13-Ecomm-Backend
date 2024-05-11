90dc08c23db8e96729faa34fc407551353577b97
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-10 22:37:54 -0400

    Implemented API routes for categories, including GET all categories with associated products, GET a single category by ID with its associated products, and POST to create a new category.

--- routes/api/category-routes.js
+++ routes/api/category-routes.js
@@ -3,18 +3,53 @@ const { Category, Product } = require('../../models');
 
 // The `/api/categories` endpoint
 
-router.get('/', (req, res) => {
-  // find all categories
-  // be sure to include its associated Products
+//http://localhost:3001/api/categories
+router.get('/', async (req, res) => {
+// try {
+  const categoryData = await  Category.findAll({
+    include: [{ model: Product} ],
+  });
+  res.status(200).json(categoryData);
+// } catch (err) {
+//   res.status(500).json(err);
+// }
 });
 
-router.get('/:id', (req, res) => {
+router.get('/:id', async (req, res) => {
   // find one category by its `id` value
   // be sure to include its associated Products
+  try {
+    const categoryData = await Category.findByPk(req.params.id, {
+      include: [{ model: Product} ],
+    });
+
+    if (!categoryData) {
+      res.status(404).json({ message: 'No category found with that id!' });
+      return;
+    }
+
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(500).json(err);
+  }
 });
 
 router.post('/', (req, res) => {
   // create a new category
+  //http://localhost:3001/api/readers/
+router.post('/', async (req, res) => {
+  /*
+  req.body={
+    username,password
+  }
+  */
+  try {
+    const categoryData = await Category.create(req.body);
+    res.status(200).json(categoryData);
+  } catch (err) {
+    res.status(400).json(err);
+  }
+});
 });
 
 router.put('/:id', (req, res) => {
