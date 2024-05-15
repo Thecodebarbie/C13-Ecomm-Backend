322196927a851f4db4cab5859d1a06ace75020d0
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-05-15 08:36:49 -0400

    reinstiall dependencies

--- config/connection.js
+++ config/connection.js
@@ -5,11 +5,9 @@ const Sequelize = require('sequelize');
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
-      host: 'localhost',
-      dialect: 'postgres',
-      dialectOptions: {
-        decimalNumbers: true,
-      },
+      host: '127.0.0.1',
+      dialect: 'mysql',
+      port: 3306
     });
 
 module.exports = sequelize;
--- routes/api/product-routes.js
+++ routes/api/product-routes.js
@@ -1,19 +1,37 @@
 const router = require('express').Router();
 const { Product, Category, Tag, ProductTag } = require('../../models');
 
-// The `/api/products` endpoint
 
-// get all products
-router.get('/', (req, res) => {
-  // find all products
-  // be sure to include its associated Category and Tag data
-});
-
-// get one product
-router.get('/:id', (req, res) => {
-  // find a single product by its `id`
-  // be sure to include its associated Category and Tag data
-});
+//http://localhost:3001/api/categories
+router.get('/', async (req, res) => {
+  // try {
+    const productData = await  Product.findAll({
+      include: [{ model: Category}, {model: Tag,through:ProductTag} ],
+    });
+    res.status(200).json(productData);
+  // } catch (err) {
+  //   res.status(500).json(err);
+  // }
+  });
+  
+  router.get('/:id', async (req, res) => {
+    // find one category by its `id` value
+    // be sure to include its associated Products
+    try {
+      const productData = await Product.findByPk(req.params.id, {
+        include: [{ model: Category},{ model: Tag, through: ProductTag} ],
+      });
+  
+      if (!productData) {
+        res.status(404).json({ message: 'No category found with that id!' });
+        return;
+      }
+  
+      res.status(200).json(productData);
+    } catch (err) {
+      res.status(500).json(err);
+    }
+  });
 
 // create new product
 router.post('/', (req, res) => {
