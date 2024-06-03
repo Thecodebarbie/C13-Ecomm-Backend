7d9803256fa7b2f0bddafca3c05831a9fb2617af
THECODEBARBIE <153017074+THEGRLCODE@users.noreply.github.com>
2024-06-03 13:32:49 -0400

    Revert db

--- config/connection.js
+++ config/connection.js
@@ -1,21 +1,13 @@
 require('dotenv').config();
-require('dotenv').config();
 
-let sequelize;
+const Sequelize = require('sequelize');
 
-if (process.env.JAWSDB_URL) {
-  sequelize = new Sequelize(process.env.JAWSDB_URL);
-} else {
-  sequelize = new Sequelize(
-    process.env.DB_NAME,
-    process.env.DB_USER,
-    process.env.DB_PW,
-    {
-      host: 'localhost',
+const sequelize = process.env.DB_URL
+  ? new Sequelize(process.env.DB_URL)
+  : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
+      host: '127.0.0.1',
       dialect: 'mysql',
-      port: 3306,
-    },
-  );
-}
+      port: 3306
+    });
 
 module.exports = sequelize;
